2898673279b1e14311dfe3f481e77f93ad26d54f
Jessie Fin <22jjf22@gmail.com>
2017-03-24 16:18:24 -0500

    Tweaking compute_forces_gpu

--- gpu.cu
+++ gpu.cu
@@ -94,20 +94,20 @@ __device__ void apply_force_gpu(particle_t &particle, particle_t &neighbor)
 
 }
 
-__global__ void compute_forces_gpu( grid_t gridlist, particle_t * particles, int n) //n is number of particles
+__global__ void compute_forces_gpu( grid_t grid, particle_t * particles, int n) //n is number of particles
 {
   // Get thread (particle) ID (one row to represent entire block)
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
   if(tid >= n) return;
 
-  linkedlist ** currBin = gridlist.grid;
-  int particlesInBin = currBin.size;
+  linkedlist_t ** currBin = grid.grid;
+  int particlesInBin = grid.size;
 
   particles[tid].ax = particles[tid].ay = 0; //initialize acceleration to 0
   
   for(int j = 0 ; j < particlesInBin ; j++) //for every particle
   {
-      apply_force_gpu(particles[tid], currBin[j]); //apply force to every other particle
+      apply_force_gpu(particles[tid], *(currBin).value); //apply force to every other particle
 
   }
 
